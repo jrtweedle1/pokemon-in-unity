64a7dc2b78793b1f921565d007909782bf1674f8
jrtweedle1 <tweedle@google.com>
2024-09-20 10:17:06 -0500

    Edit sample scene and initialize player movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
